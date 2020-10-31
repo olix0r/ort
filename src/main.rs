97104926ce7dab7908325d30c50315ccaa9862f2
Oliver Gould <ver@buoyant.io>
2020-10-31 03:40:50 +0000

    Add a Helm tempalte
    And remove the `controller` implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![deny(warnings, rust_2018_idioms)]
 
-#[cfg(feature = "controller")]
-use ortiofay_controller as controller;
 #[cfg(feature = "load")]
 use ortiofay_load as load;
 #[cfg(feature = "server")]
@@ -11,9 +9,6 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 #[structopt(about = "Load harness")]
 enum Ortiofay {
-    #[cfg(feature = "controller")]
-    Controller(controller::Controller),
-
     #[cfg(feature = "load")]
     Load(load::Load),
 
@@ -26,12 +21,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     tracing_subscriber::fmt::init();
 
     let cmd = Ortiofay::from_args();
-
-    #[cfg(feature = "controller")]
-    if let Ortiofay::Controller(c) = cmd {
-        c.run().await?;
-        return Ok(());
-    }
 
     #[cfg(feature = "load")]
     if let Ortiofay::Load(l) = cmd {
