e5371351a79129a13ed88088f21593022628859c
Oliver Gould <ver@buoyant.io>
2020-11-24 18:23:11 +0000

    reconnect: Prevent reconnecting seperately in each clone

--- core/src/reconnect.rs
+++ core/src/reconnect.rs
@@ -1,5 +1,6 @@
 use crate::{Error, MakeOrt, Ort, Reply, Spec};
-use tokio::time;
+use std::sync::Arc;
+use tokio::{sync::Mutex, time};
 use tracing::debug;
 
 #[derive(Clone)]
@@ -13,7 +14,7 @@ pub struct MakeReconnect<M> {
 pub struct Reconnect<T, M, O> {
     target: T,
     make: MakeReconnect<M>,
-    inner: O,
+    inner: Arc<Mutex<O>>,
 }
 
 impl<M> MakeReconnect<M> {
@@ -22,6 +23,28 @@ impl<M> MakeReconnect<M> {
             inner,
             timeout,
             backoff,
+        }
+    }
+}
+
+impl<M> MakeReconnect<M>
+where
+{
+    async fn make_inner<T>(&mut self, target: T) -> Result<M::Ort, Error>
+    where
+        T: Clone + Send + Sync + 'static,
+        M: MakeOrt<T> + Clone,
+    {
+        let mut backoff = self.backoff;
+        loop {
+            let connect = self.inner.make_ort(target.clone());
+            match time::timeout(self.timeout, connect).await {
+                Ok(Ok(inner)) => return Ok(inner),
+                _ => {}
+            }
+
+            time::delay_for(backoff).await;
+            backoff *= 2;
         }
     }
 }
@@ -35,33 +58,8 @@ where
     type Ort = Reconnect<T, M, M::Ort>;
 
     async fn make_ort(&mut self, target: T) -> Result<Self::Ort, Error> {
-        let make = self.clone();
-        let mut backoff = self.backoff;
-        loop {
-            let connect = self.inner.make_ort(target.clone());
-            let timeout = time::delay_for(self.timeout);
-            tokio::select! {
-                res = connect => {
-                    match res {
-                        Ok(inner) => {
-                            return Ok(Reconnect {
-                                target,
-                                inner,
-                                make,
-                            })
-                        }
-                        Err(_) => {
-                            time::delay_for(backoff).await;
-                            backoff *= 2;
-                        }
-                    }
-                }
-                _ = timeout => {
-                    time::delay_for(backoff).await;
-                    backoff *= 2;
-                }
-            }
-        }
+        let inner = self.make_inner(target.clone()).await?;
+        return Ok(Reconnect { target, inner: Arc::new(Mutex::new(inner)), make: self.clone(), });
     }
 }
 
@@ -72,14 +70,13 @@ where
     M: MakeOrt<T>,
 {
     async fn ort(&mut self, spec: Spec) -> Result<Reply, Error> {
-        // FIXME: This is AFU. Instances are cloned so this does not do the
-        // right thing.
+        let mut inner = self.inner.lock().await;
         loop {
-            self.inner = match self.inner.ort(spec.clone()).await {
+            match inner.ort(spec.clone()).await {
                 Ok(reply) => return Ok(reply),
                 Err(error) => {
                     debug!(%error, "Rebuilding client");
-                    self.make.make_ort(self.target.clone()).await?.inner
+                    *inner = self.make.make_inner(self.target.clone()).await?;
                 }
             };
         }
